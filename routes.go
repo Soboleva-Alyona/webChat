bca986cc7441e62e4fd182969cad55d481d4dcc3
Soboleva-Alyona <alyonasoboleva1112@mail.ru>
2022-04-07 22:52:40 +0300

    some changes

--- routes.go
+++ routes.go
@@ -8,29 +8,14 @@ func initializeRouts() {
 
 	userRoutes := router.Group("/")
 	{
-		// Handle the GET requests at /u/login
-		// Show the login page
-		// Ensure that the user is not logged in by using the middleware
 		userRoutes.GET("/enter", enterHandler)
 
-		//// Handle POST requests at /u/login
-		//// Ensure that the user is not logged in by using the middleware
 		userRoutes.POST("/enter", doEnterHandler)
 
 		userRoutes.POST("/add_friend", doAddFriendHandler)
-		//
-		//// Handle GET requests at /u/logout
-		//// Ensure that the user is logged in by using the middleware
-		//userRoutes.GET("/logout", ensureLoggedIn(), logout)
 
-		// Handle the GET requests at /u/register
-		// Show the registration page
-		// Ensure that the user is not logged in by using the middleware
 		userRoutes.GET("/register", registerHandler)
 
-		//
-		//// Handle POST requests at /u/register
-		//// Ensure that the user is not logged in by using the middleware
 		userRoutes.POST("/register", doRegisterHandler)
 
 		userRoutes.GET("/logout", doLogOutHandler)
